f5f7484f84e95d8c13d70998273b5663989463e7
Luis Fernando <nando_srs@hotmail.com>
2022-07-22 10:42:37 -0300

    ActiveMQ adicionando na queue insert (Post)

--- produtos/src/main/java/com/luisfernando/produtos/api/ProdutoApi.java
+++ produtos/src/main/java/com/luisfernando/produtos/api/ProdutoApi.java
@@ -1,8 +1,11 @@
 package com.luisfernando.produtos.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luisfernando.produtos.model.Produto;
 import com.luisfernando.produtos.repository.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +18,12 @@ public class ProdutoApi {
     @Autowired
     private ProdutosRepository repository;
 
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping
     public List<Produto> all() {
         return repository.findAll();
@@ -26,12 +35,15 @@ public class ProdutoApi {
     }
 
     @PostMapping
-    public Produto insert(@RequestBody Produto produto){
-        return repository.save(produto);
+    public Produto insert(@RequestBody Produto produto) throws JsonProcessingException {
+        repository.save(produto);
+        var produtoJson = objectMapper.writeValueAsString(produto);
+        jmsTemplate.convertAndSend("queue.produto.insert", produtoJson);
+        return produto;
     }
 
     @DeleteMapping
-    public void delete (@PathVariable("id") Long id){
+    public void delete(@PathVariable("id") Long id) {
         repository.deleteById(id);
     }
 
